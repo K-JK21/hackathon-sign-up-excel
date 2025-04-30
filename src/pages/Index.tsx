7fc16cadbd5b5342e01e87a20d632986ccd11aba
Jibek <96116894+K-JK21@users.noreply.github.com>
2025-04-30 17:04:59 +0600

    yuy

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -18,6 +18,7 @@ const Index = () => {
             <div className="flex">
               <div className="flex-shrink-0 flex items-center">
                 <span className="text-xl font-bold text-violet-600">ЖаңырганАкыл</span>
+                <span className="text-xl font-bold text-violet-600">ЖаңырганАкыл</span>
               </div>
             </div>
             <div className="flex items-center space-x-4">
